4085a1e781772ebab8c50c51d7b0ea4cf7487102
Nugroho Wardi <nugroho@maleo.social>
2024-12-26 22:55:31 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-amber-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Tailwind CSS!
+      </h1>
+    </main>
+  )
+}
